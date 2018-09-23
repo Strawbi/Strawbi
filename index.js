56b8bde7dc89a2863d652663202a15445f9fb5c4
Strawbi <43515123+Strawbi@users.noreply.github.com>
2018-09-23 12:53:04 +0100

    Update index.js

--- index.js
+++ index.js
@@ -1,17 +1,15 @@
 const Discord = require('discord.js');
-const bot = new Discord.Client();
+const client = new Discord.Client();
 
-
-bot.on('message', function(message){
-    if (message.content == 'cmds')
-    {
-        message.reply('I currently do not have any commands.');
-    } 
+client.on('ready', () => {
+    console.log('I am ready!');
 });
 
+client.on('message', message => {
+    if (message.content === 'cmds') {
+    	message.reply('I currently do not have any commands.');
+  	}
+});
 
-
-
-
-
-bot.login('NDkzMDcyNjI4Nzk4NzE3OTUz.DofpdA.uiuSuP-WIp1ssv3M2ZPpZbS1EFU');
+// THIS  MUST  BE  THIS  WAY
+client.login(process.env.BOT_TOKEN);
